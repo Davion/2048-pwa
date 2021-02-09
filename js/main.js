0d704c58e43d91255f6a29106fb4cad9f34815e2
Klajdi Hoxha <klainti95@gmail.com>
2021-02-09 12:16:57 +0100

    spawn random empty tile

--- js/main.js
+++ js/main.js
@@ -6,40 +6,65 @@ const gameBoard = document.querySelector(".game-board");;
 const startingNums = [2, 2, 2, 4];
 const gameTiles = Object.values(gameBoard.childNodes).filter(tile => tile.nodeName !== "#text");
 
-let startingBoard = [];
-
-// console.log(startingNums[Math.floor(Math.random() * 2)]);
+// holds game board values that will be applied to gameTiles
+let gameBoardValues = [];
+// holds empty tiles in relation to gameTiles
+let emptyTiles = [];
 
 // new game function that sets up the board and score
 newGameBtn.addEventListener("click", newGame);
 
-// function that generates a 2 or a 4 in random
-
-// funtion that spawns a tile with 2 or 4 on a previously empty tile
-
 // function that moves on keypress or swipe (game logic)
-
+gameBoard.addEventListener("keydown", e => boardMove(e.code));
 
 function newGame(){
-    // startingBoard = [];
-    // startingBoard[5] = 8;
-    // startingBoard[12] = 16;
-    if(startingBoard.length < 16){
-        let idx = randomEmptyTile(startingBoard);
-        console.log("The index: " + idx);
-        startingBoard[idx] = startingNums[Math.floor(Math.random() * 4)];
-    }
+    
+    gameBoardValues = [];
+    const firstIdx = randomTileSpawn();
+    const secondIdx = randomTileSpawn();
+    gameBoardValues[firstIdx] = 2;
+    gameBoardValues[secondIdx] = twoOrFour();
     for(let i = 0; i < gameTiles.length; i++){
-        gameTiles[i].textContent = startingBoard[i];
+        gameTiles[i].textContent = gameBoardValues[i];
     }
 
 }
 
-function randomEmptyTile(arr){
-    let randomNr = Math.floor(Math.random() * 15);
-    console.log(randomNr);
-    if(arr[randomNr] === undefined) 
-        return randomNr;
-    else
-        return randomEmptyTile(arr);
+// returns a random index of one of the remaining empty tiles on the game board
+function randomTileSpawn(){
+    emptyTiles = [];
+
+    for(let i = 0; i < gameTiles.length; i++){
+        if(gameTiles[i].textContent === undefined || 
+            gameTiles[i].textContent === "") emptyTiles.push(i);
+    }
+
+    if(emptyTiles.length > 0){
+        return emptyTiles[getRandomEmptyTile(emptyTiles.length)];
+    }
+}
+
+// returns a random number based on a dynamic range of values
+function getRandomEmptyTile(arrLen){
+    return Math.floor(Math.random() * arrLen);
+}
+
+// function that generates a 2 or a 4 in random
+function twoOrFour(){
+    return startingNums[Math.floor(Math.random() * startingNums.length)];
+}
+
+function boardMove(keyCode){
+    switch(keyCode){
+        case "ArrowUp":
+            break;
+        case "ArrowRight":
+            break;
+        case "ArrowDown":
+            break;
+        case "ArrowLeft":
+            break;
+        default:
+            break;
+    }
 }
