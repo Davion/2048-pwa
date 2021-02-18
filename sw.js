723eb277d08c6910bcf9418d683a3715cc60847c
Klajdi Hoxha <klainti95@gmail.com>
2021-02-18 13:00:06 +0100

    bugfix service worker assets array

--- sw.js
+++ sw.js
@@ -1,51 +1,51 @@
 const staticCascheName = "2048-app-static-v1";
 const assets = [
-    "/2048-pwa/",
-    "/2048-pwa/index.html",
-    "/2048-pwa/css/normalize.css",
-    "/2048-pwa/css/reset.css",
-    "/2048-pwa/css/styles.css",
-    "/2048-pwa/js/main.js",
-    "/2048-pwa/js/swiped-events.min.js",
-    "/2048-pwa/icons/ZOAB-icon.png",
+  "/2048-pwa/",
+  "/2048-pwa/index.html",
+  "/2048-pwa/css/normalize.css",
+  "/2048-pwa/css/reset.css",
+  "/2048-pwa/css/styles.css",
+  "/2048-pwa/js/main.js",
+  "/2048-pwa/js/swiped-events.min.js",
+  "/2048-pwa/icons/ZOAB-icon.png"
 ];
 
 self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-      caches.open(staticCascheName).then(cache => {
-        cache.addAll(assets);
-        console.log("service worker installed -- assets cached");
-      }).catch(err => console.log("issue caching assets on install - ", err))
-    );
-  });
+  installEvent.waitUntil(
+    caches.open(staticCascheName).then(cache => {
+      cache.addAll(assets);
+      console.log("service worker installed -- assets cached");
+    }).catch(err => console.log("issue caching assets on install - ", err))
+  );
+});
   
-  self.addEventListener("activate", activateEvent => {
-    activateEvent.waitUntil(
-      caches.keys().then(keys => {
-        console.log("service worker activated");
-        return Promise.all(keys
-          .filter(key => key !== staticCascheName)
-          .map(key => caches.delete(key))
-        )
-      }).catch(err => console.log("issue getting cacheKeys on activate - ", err))
-    );
-  });
-  
-  self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.open(staticCascheName).then(cache => {
-        console.log("fetching from service worker");
-        return cache.match(fetchEvent.request).then(res => res || fetch(fetchEvent.request)
-          .then(networkRes => {
-            console.log("puting network response to cache");
-            cache.put(fetchEvent.request, networkRes.clone());
-            return networkRes;
-          })
-        ).catch(() => {
-          if(fetchEvent.request.url.indexOf(".html") > -1 || fetchEvent.request.url === "https://github.com/Davion"){
-            return caches.match("/todo-app-js-pwa/fallback.html");
-          }
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(keys => {
+      console.log("service worker activated");
+      return Promise.all(keys
+        .filter(key => key !== staticCascheName)
+        .map(key => caches.delete(key))
+      )
+    }).catch(err => console.log("issue getting cacheKeys on activate - ", err))
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.open(staticCascheName).then(cache => {
+      console.log("fetching from service worker");
+      return cache.match(fetchEvent.request).then(res => res || fetch(fetchEvent.request)
+        .then(networkRes => {
+          console.log("puting network response to cache");
+          cache.put(fetchEvent.request, networkRes.clone());
+          return networkRes;
         })
-      }).catch(err => console.log("issue opening cache on fetch", err))
-    );
-  });
+      ).catch(() => {
+        if(fetchEvent.request.url.indexOf(".html") > -1 || fetchEvent.request.url === "https://github.com/Davion"){
+          return caches.match("/todo-app-js-pwa/fallback.html");
+        }
+      })
+    }).catch(err => console.log("issue opening cache on fetch", err))
+  );
+});
